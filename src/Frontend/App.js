a68fb66a30f536afc6a0de6a4cfc72cb126defca
Nicolas Cabrera <nicolascc1@hotmail.com>
2020-03-16 15:03:24 +0000

    login with google test

--- src/Frontend/App.js
+++ src/Frontend/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import './App.css';
 import Test from './Test';
+import GoogleLogin from 'react-google-login';
 
 function App() {
-  return (
-    <div className="App">
-		<h1>Helloo and welcome</h1>
-		<Test />
-    </div>
-  );
+
+	const responseGoogle = (response) => {
+		console.log(response);
+	}
+
+	return (
+		<div className="App">
+			<h1>Helloo and welcome</h1>
+			<Test />
+
+			<GoogleLogin
+				clientId="436450418933-1gb19igjut0e2s2dag5g7vpq7ink236p.apps.googleusercontent.com"
+				buttonText="Login"
+				onSuccess={responseGoogle}
+				onFailure={responseGoogle}
+				cookiePolicy={'single_host_origin'}
+			/>
+		</div>
+	);
 }
 
 export default App;
