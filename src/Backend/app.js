396ff54daa0c3185d522f887d3a7d964834176b7
Nicolas Cabrera <nicolascc1@hotmail.com>
2020-03-16 14:36:31 +0000

    test 2.0 potential fix

--- src/Backend/app.js
+++ src/Backend/app.js
@@ -4,14 +4,16 @@ const path = require('path');
 const port = process.env.PORT || 3001;
 const getTest = require('./test');
 
-app.get('*', function (req, res) {
-	res.sendFile(path.join(__dirname, '../../build', 'index.html'));
-  });
-
-app.get('/', (req, res) => res.send('Hello welcome to the backend!!'));
-
 app.get('/rest/movies', (req, res) => {
 	res.send(getTest());
 }); 
 
+app.use(express.static(path.join(__dirname, '../../build')));
+
+app.get('/', (req, res) => res.send('Hello welcome to the backend!!'));
+
 app.listen(port, () => console.log(`This is port ${port}!`));
+
+app.get('*', function (req, res) {
+	res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+  });
