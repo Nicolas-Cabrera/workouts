dd53406c400b7331a2dddbcc7277b32f94ec03df
Nicolas Cabrera <nicolascc1@hotmail.com>
2020-03-31 16:21:30 +0100

    working on the timer

--- src/Frontend/Components/Timer/Timer.js
+++ src/Frontend/Components/Timer/Timer.js
@@ -1,15 +1,51 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useInterval, useEffect } from 'react';
 import './Timer.css';
 
 export default function Timer() {
 
-	const [ timer, setTimer ] = useState(30);
+	const [ timer, setTimer ] = useState(2);
+	const [ isActive, setIsActive ] = useState(false);
 
-	// useEffect(() => {
-	// 	setTimeout(() => {
-	// 		setTimer(timer - 1);
-	// 	}, 1000);
-	// })
+	function timerSetter(e) {
+		const value = e.target.value;
+		setTimer(+value);
+	}
+
+	function fiveSetter(e) {
+		const five = e.target.value;
+		if(five === '-5') {
+			setTimer(timer - 5);
+		} else if(five === '+5') {
+			setTimer(timer + 5);
+		}
+	}
+
+	function turnToMins(nums) {
+		let mins = Math.floor(nums / 60);
+		let secs = nums % 60;
+		if(secs < 10) {
+			return `${mins}:0${secs}`;
+		} else {
+			return `${mins}:${secs}`;
+		}
+	}
+
+	function starTimer() {
+		setIsActive(!isActive);
+	}
+
+	useEffect(() => {
+		let interval = null;
+		console.log(timer);
+		if(isActive) {
+			interval = setInterval(() => {
+				setTimer(timer => timer - 1);
+			}, 1000);
+		} else if(!isActive) {
+			clearInterval(interval);
+		} 
+		return () => clearInterval(interval);
+	}, [isActive, timer]);
 
 	return (
 		<div>
@@ -18,16 +54,29 @@ export default function Timer() {
 			</div>
 			<div className='main-timer'>
 				<div>
-					<button className='five-button'>-5</button>
+					<button className='five-button' value='-5' onClick={(e) => fiveSetter(e)}>-5</button>
 				</div>
 				<div className='timer-clock'>
-					<h1>00:30</h1>
+					<h1>{turnToMins(timer)}</h1>
 				</div>
-				<div>
-					<button className='five-button'>+5</button>
-				</div>
+				<button className='five-button' value='+5' onClick={(e) => fiveSetter(e)}>+5</button>
 			</div>
-			<p>Time is: {timer}</p>
+			<div>
+			<button className='start-button' onClick={() =>starTimer()}>{
+				isActive ? 'Pause' : 'Start'
+			}</button>
+			</div>
+			<div className='grid-container'>
+				<button className='num-buttons' value='20' onClick={(e) => timerSetter(e)}>20</button>
+				<button className='num-buttons' value='30' onClick={(e) => timerSetter(e)}>30</button>
+				<button className='num-buttons' value='45' onClick={(e) => timerSetter(e)}>45</button>
+				<button className='num-buttons' value='60' onClick={(e) => timerSetter(e)}>60</button>
+				<button className='num-buttons' value='90' onClick={(e) => timerSetter(e)}>90</button>
+				<button className='num-buttons' value='120' onClick={(e) => timerSetter(e)}>120</button>
+				<button className='num-buttons' value='135' onClick={(e) => timerSetter(e)}>135</button>
+				<button className='num-buttons' value='150' onClick={(e) => timerSetter(e)}>150</button>
+				<button className='num-buttons' value='180' onClick={(e) => timerSetter(e)}>180</button>
+			</div>
 		</div>
 	);
 }
