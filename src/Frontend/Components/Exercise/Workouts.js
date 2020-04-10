c8bc8abbb1ae65383c606cfa4cdd29c2055127a1
Nicolas Cabrera <nicolascc1@hotmail.com>
2020-04-10 14:17:43 +0100

    added mapping for display workout names

--- src/Frontend/Components/Exercise/Workouts.js
+++ src/Frontend/Components/Exercise/Workouts.js
@@ -5,12 +5,17 @@ import back from '../../img/back.png';
 
 export default function Workouts(props) {
 
-	const [num, setNum] = useState(props.location.state.num)
+	const [ num ] = useState(props.location.state.num)
+	const [ muscle ] = useState(props.location.state.muscle)
+	const [ workouts, setWorkouts ] = useState();
 
 	useEffect(() => {
 		fetch('/rest/workouts')
 			.then(response => response.json())
-			.then(res => console.log(res[num]))
+			.then(res => {
+				console.log(res[num]);
+				setWorkouts(res[num].Shoulders);
+			})
 	}, []);
 
 	return (
@@ -19,8 +24,10 @@ export default function Workouts(props) {
 				<Link to='/Main' ><img className='back' src={back} alt='back' /></Link>
 				<h1>{props.location.state.muscle}</h1>
 			</div>
-			<div>
-				
+			<div className='grid-workouts'>
+				{
+					workouts ? workouts.map((a) => <h2 className='grid-item'>{a.name}</h2>) : null
+				}
 			</div>
 		</div>
 	);
