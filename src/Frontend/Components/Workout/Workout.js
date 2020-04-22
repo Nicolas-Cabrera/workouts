a5843771b28d7c9d1db625b8299328ecad259c51
Nicolas Cabrera <nicolascc1@hotmail.com>
2020-04-22 13:56:09 +0100

    Designed GUI for recording workout

--- src/Frontend/Components/Workout/Workout.js
+++ src/Frontend/Components/Workout/Workout.js
@@ -30,9 +30,31 @@ export default function Workout() {
 					<h1>Record Workout</h1>
 				</div>
 				<div className='workout-section'>
-					<div>
-						<h2 className='intro'>Ready to start</h2>
+					<div className='workout-block'>
+						<div className='workouts-top'>
+							<form className='select-muscle'>
+								<select>
+									<option>Bench Press</option>
+									<option>Cable Fly</option>
+									<option>Dips</option>
+								</select>
+							</form>
+							<h6 className='date'>22nd April 2020</h6>
+						</div>
+						<div className='headers'>
+							<h4 className='header-item'>SET</h4>
+							<h4 className='header-item'>WEIGHT(kg)</h4>
+							<h4 className='header-item'>REPS</h4>
+						</div>
+						<div className='sets'>
+							<h4 className='sets-item'>1</h4>
+							<h4 className='sets-item'>55</h4>
+							<h4 className='sets-item'>10</h4>
+						</div>
+						<button>Add new set</button>
 					</div>
+					<button>Add New Exercise</button>
+					<button>Finish Workout</button>
 				</div>
 			</div>
 		);
