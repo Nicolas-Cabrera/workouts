f622a2d0d8097f60cd80c4a6456a56f184785d33
Nicolas Cabrera <nicolascc1@hotmail.com>
2020-04-22 13:17:08 +0100

    started the workout tab

--- src/Frontend/Components/Workout/Workout.js
+++ src/Frontend/Components/Workout/Workout.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Workout.css';
 
 export default function Workout() {
-	return (
-		<div>
-			<div className='title'>
-				<h1>Record Workout</h1>
+
+	const [ show, setShow ] = useState(true);
+
+	function showHide() {
+		setShow(!show);
+	}
+
+	if(show) {
+		return (
+			<div>
+				<div className='title'>
+					<h1>Record Workout</h1>
+				</div>
+				<div className='workout-section'>
+					<div>
+						<h2 className='intro'>Stretch and warm up before starting, remember you can't train if you are injured...</h2>
+						<button className='start-button' onClick={() => showHide()}>START</button>
+					</div>
+				</div>
 			</div>
-		</div>
-	);
+		);
+	} else {
+		return (
+			<div>
+				<div className='title'>
+					<h1>Record Workout</h1>
+				</div>
+				<div className='workout-section'>
+					<div>
+						<h2 className='intro'>Ready to start</h2>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
