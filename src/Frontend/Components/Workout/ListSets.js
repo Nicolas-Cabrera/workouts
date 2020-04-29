425428c5ac8f468986da3dbfa9d258a295f25c9d
Nicolas Cabrera <nicolascc1@hotmail.com>
2020-04-29 14:14:21 +0100

    created object to display weight and reps

--- src/Frontend/Components/Workout/ListSets.js
+++ src/Frontend/Components/Workout/ListSets.js
@@ -4,6 +4,7 @@ import './Workout.css';
 export default function ListSets(props) {
 	const weight = props.weight;
 	const rep = props.rep;
+	const obj = [{"Weight":weight, "Reps":rep}];
 	const listWeight = weight.map((item, index) => {
 		return <div className='list' key={index}>
 			<span className='set'>{index + 1}</span> <span className='weight'>{item}</span>
@@ -15,6 +16,8 @@ export default function ListSets(props) {
 			<span>{item}</span>
 		</div>
 	})
+
+	console.log(obj);
 
 	return <div className='history'>
 		<div>{listWeight}</div>
