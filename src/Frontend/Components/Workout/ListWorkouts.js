5856a94ea3222f84fc9d30bda4eeea7d2b795dfa
Nicolas Cabrera <nicolascc1@hotmail.com>
2020-05-05 11:02:23 +0100

    Added and fixed finish exercise button

--- src/Frontend/Components/Workout/ListWorkouts.js
+++ src/Frontend/Components/Workout/ListWorkouts.js
@@ -3,27 +3,28 @@ import ListSets from './ListSets';
 
 export default function ListWorkouts() {
 
-	const [ weighthistory, setWeightHistory ]  = useState([]);
-	const [ rephistory, setRepHistory ]  = useState([]);
-	const [ fullWorkout, setFullWorkout ] = useState([]);
-	const [ rep, setRep ] = useState();
-	const [ weight,setWeight ] = useState();
-	const [ num, setNum ] = useState(1);
-	const [ obj, setObj ] = useState([]);
+	const [weighthistory, setWeightHistory] = useState([]);
+	const [rephistory, setRepHistory] = useState([]);
+	const [fullWorkout, setFullWorkout] = useState([]);
+	const [finish, setFinish] = useState(false);
+	const [rep, setRep] = useState();
+	const [weight, setWeight] = useState();
+	const [num, setNum] = useState(1);
+	const [obj, setObj] = useState([]);
 
 	function handleInput(e, index) {
-		if(e && index === 1) {
+		if (e && index === 1) {
 			setWeight(e.target.value);
-		} else if(e && index === 2) {
+		} else if (e && index === 2) {
 			setRep(e.target.value);
 		}
 	}
 
 	function addRep() {
-		if(weight && rep) {
+		if (weight && rep) {
 			const newWeight = weight;
 			const newRep = rep;
-			if(newWeight !== '' && newRep !== '') {
+			if (newWeight !== '' && newRep !== '') {
 				const newWeights = [...weighthistory, newWeight];
 				const newReps = [...rephistory, newRep];
 				setWeightHistory(newWeights);
@@ -35,27 +36,50 @@ export default function ListWorkouts() {
 		}
 	}
 
-	return (
-		<div className='section'>
-			<form className='select-exercise'>
-				<select>
-					<option>test for muscle workout</option>
-				</select>
-			</form>
-			<div className='headers'>
-				<h4 className='header-item'>SET</h4>
-				<h4 className='header-item'>WEIGHT(kg)</h4>
-				<h4 className='header-item-reps'>REPS</h4>
-			</div>
-			<ListSets weight={weighthistory} rep={rephistory} />
-			<div>
-				<form className='sets'>
-					<label className='sets-item'>{num}</label>
-					<input type='number' className='sets-item' value={weight} onChange={(e) => handleInput(e, 1)} ></input>
-					<input type='number' className='sets-item' value={rep} onChange={(e) => handleInput(e, 2)} ></input>
+	function finishSet() {
+		setFinish(true);
+	}
+
+	if (!finish) {
+		return (
+			<div className='section'>
+				<form className='select-exercise'>
+					<select>
+						<option>test for muscle workout</option>
+					</select>
 				</form>
-			</div>
-			<button onClick={() => addRep()}>Add new set</button>
-		</div >
-	);
+				<div className='headers'>
+					<h4 className='header-item'>SET</h4>
+					<h4 className='header-item'>WEIGHT(kg)</h4>
+					<h4 className='header-item-reps'>REPS</h4>
+				</div>
+				<ListSets weight={weighthistory} rep={rephistory} />
+				<div>
+					<form className='sets'>
+						<label className='sets-item'>{num}</label>
+						<input type='number' className='sets-item' value={weight} onChange={(e) => handleInput(e, 1)} ></input>
+						<input type='number' className='sets-item' value={rep} onChange={(e) => handleInput(e, 2)} ></input>
+					</form>
+				</div>
+				<button onClick={() => addRep()}>Add new set</button>
+				<button onClick={() => finishSet()}>Finish Exercise</button>
+			</div >
+		);
+	} else {
+		return (
+			<div className='section'>
+				<form className='select-exercise'>
+					<select>
+						<option>test for muscle workout</option>
+					</select>
+				</form>
+				<div className='headers'>
+					<h4 className='header-item'>SET</h4>
+					<h4 className='header-item'>WEIGHT(kg)</h4>
+					<h4 className='header-item-reps'>REPS</h4>
+				</div>
+				<ListSets weight={weighthistory} rep={rephistory} />
+			</div >
+		);
+	}
 }
