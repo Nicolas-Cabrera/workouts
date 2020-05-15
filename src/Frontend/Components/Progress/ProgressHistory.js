4da031fc69e8fe1cce2cd17f4bba4b444b4d48e8
Nicolas Cabrera <nicolascc1@hotmail.com>
2020-05-15 15:41:39 +0100

    Date added to progress history component

--- src/Frontend/Components/Progress/ProgressHistory.js
+++ src/Frontend/Components/Progress/ProgressHistory.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { useHistory } from 'react-router';
 import back from '../../img/back.png';
 
-export default function ProgressHistory({date, month, year}) {
+export default function ProgressHistory(props) {
 
+	const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Sept', 'Oct', 'Nov', 'Dec'];
 	const history = useHistory();
+	const date = props.location.state.date;
+	const month = props.location.state.month;
+	const year = props.location.state.year;
 
 	function goBackHandler() {
 		history.goBack();
 	}
-
-	console.log(date);
 
 	return (
 		<div>
@@ -18,7 +20,7 @@ export default function ProgressHistory({date, month, year}) {
 				<button className='back-button'><img src={back} alt='back' onClick={goBackHandler}/></button>
 				<h2>Progress History</h2>
 			</div>
-			<h3>{date} {month} {year}</h3>
+			<h3>{date} {months[month]} {year}</h3>
 		</div>
 	);
 }
